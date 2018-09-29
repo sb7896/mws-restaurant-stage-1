609f62b749d87a3d3c91da046738522315b8d4ae
Swapnil Bangare <sappystuff@gmail.com>
2018-09-30 03:55:47 +0530

    Improved build tasks
    - Gzipped files will be served if available.

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,11 @@ var uglifycss = require('gulp-uglifycss');
 var gzip = require('gulp-gzip');
 var webp = require('gulp-webp');
 var runSequence = require('run-sequence');
+var connect = require('gulp-connect');
+var connectGzip = require('connect-gzip')
+// var connectGzipStatic = require('connect-gzip-static');
 
+/*=======================Run this task for development purposes=========================**/
 gulp.task('default', ['clean', 'copy-html', 'copy-icons', 'jpgtowebp',
         'styles', 'scripts', 'service-worker', 'copy-manifest'
     ],
@@ -25,24 +29,22 @@ gulp.task('default', ['clean', 'copy-html', 'copy-icons', 'jpgtowebp',
         });
     });
 
-gulp.task('dist', function (done) {
-
-    runSequence('clean', [
-        'copy-html',
-        'copy-icons',
-        'jpgtowebp',
-        'styles',
-        'scripts-dist',
-        'service-worker',
-        'copy-manifest'
-    ], function () {
-        done();
-    });
-
-    /* browserSync.init({
-        server: './dist'
-    }); */
+gulp.task('watch', function () {
+    gulp.watch('./*.html', ['copy-html']);
+    gulp.watch('css/**/*.css', ['styles']);
+    gulp.watch('js/**/*.js', ['scripts']);
+    gulp.watch('./serviceworker.js', ['service-worker']);
 });
+
+/*=======================Run this task for production purposes=========================**/
+gulp.task('dist', function (done) {
+    runSequence('clean', ['copy'], ['gzip'], ['start', 'watch'], done);
+});
+
+/*=======================Copies all resources to /dist folder=========================**/
+gulp.task('copy', ['copy-html', 'copy-icons', 'jpgtowebp', 'styles', 'scripts-dist',
+    'service-worker', 'copy-manifest'
+]);
 
 gulp.task('clean', function () {
     return del('dist/**', {
@@ -52,7 +54,8 @@ gulp.task('clean', function () {
 
 gulp.task('copy-html', function () {
     gulp.src('./*.html')
-        .pipe(gulp.dest('./dist'));
+        .pipe(gulp.dest('./dist'))
+        .pipe(connect.reload());
 });
 
 gulp.task('copy-icons', function () {
@@ -73,6 +76,7 @@ gulp.task('styles', function () {
     gulp.src('css/**/*.css')
         .pipe(uglifycss())
         .pipe(gulp.dest('dist/css'))
+        .pipe(connect.reload());
 });
 
 gulp.task('scripts', function () {
@@ -85,16 +89,49 @@ gulp.task('scripts', function () {
 gulp.task('scripts-dist', function () {
     gulp.src('js/**/*.js')
         .pipe(uglifyjs())
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(connect.reload());
 });
 
 gulp.task('service-worker', function () {
     return gulp.src('./serviceworker.js')
         .pipe(uglifyjs())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(connect.reload());
 });
 
 gulp.task('copy-manifest', function () {
     return gulp.src('./manifest.json')
         .pipe(gulp.dest('dist'));
+});
+
+/*=======================Compress files to improve page load  =========================**/
+
+gulp.task('gzip', ['gzip-js', 'gzip-css']);
+
+gulp.task('gzip-js', function () {
+    gulp.src('dist/js/**/*.js')
+        .pipe(gzip())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('gzip-css', function () {
+    gulp.src('dist/css/**/*.css')
+        .pipe(gzip())
+        .pipe(gulp.dest('dist/css'));
+});
+
+/*=======================Starts the server in /dist directory=========================**/
+gulp.task('start', function () {
+    // var oneDay = 86400000;
+    connect.server({
+        root: 'dist',
+        port: 8000,
+        livereload: false,
+        middleware: function () {
+            return [
+                connectGzip.gzip()
+            ];
+        }
+    });
 });
